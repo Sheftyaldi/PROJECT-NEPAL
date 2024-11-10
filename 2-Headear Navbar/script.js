87250e4e31c0bc6c263a0723fbc8b9c2a914fbb8
Sheftyaldi <130757857+Sheftyaldi@users.noreply.github.com>
2024-11-10 21:07:15 +0700

    Update v.3

--- 2-Headear Navbar/script.js
+++ 2-Headear Navbar/script.js
@@ -2,40 +2,11 @@ const container = document.querySelector(".container");
 const heading = document.querySelector(".heading");
 const closeNav = document.querySelector(".close");
 const items = document.querySelectorAll(".item");
+const menu = document.querySelector(".menu");
+const listProfil = document.querySelector(".list-profil");
+const listMenu = document.querySelector(".list-menu");
 
-// fungsi Sidebar
-function showSideBar(e) {
-  if (e.target.classList.toggle(".close")) {
-    heading.style.flexDirection = "column";
-    e.target.innerText = "arrow_forward_ios";
-    container.style.width = "75px";
-  } else {
-    heading.style.flexDirection = "row";
-    e.target.innerText = "arrow_back_ios";
-    container.style.width = "100%";
-  }
-}
-
-// Fungsi mengilangkan keterangan Icon
-function closeItems() {
-  items.forEach((e) => {
-    const parentE = e.parentElement;
-    if (!e.classList.toggle("inline")) {
-      e.style.display = "inline";
-      parentE.style.width = "95%";
-      parentE.style.borderRadius = "1em";
-    } else {
-      e.style.display = "none";
-      parentE.style.width = "55px";
-      parentE.style.borderRadius = ".2em";
-      console.log(parentE);
-    }
-  });
-}
-
-closeNav.addEventListener("click", (e) => {
-  // Untuk menutup / buka sideBarnya
-  showSideBar(e);
-  // Untuk hanya Icon item yang tampil
-  closeItems();
+menu.addEventListener("click", () => {
+  listProfil.classList.toggle("tutup");
+  listMenu.classList.toggle("tutup");
 });
